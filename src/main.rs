d7e47c1aa3460c6f86e5566546024115f96bcaba
andy salerno <andysalerno@gmail.com>
2018-11-25 09:35:31 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+extern crate rusqlite;
+extern crate sodiumoxide;
+
+mod pass_db;
+
+use sodiumoxide::crypto::secretbox;
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    sodiumoxide::init().expect("didn't init properly");
+    Ok(())
+}
+
+fn encrypt() {
+    let key = secretbox::gen_key();
+    let nonce = secretbox::gen_nonce();
+    println!("{:?}", nonce);
+    println!("{:?}", key);
+    let cipher_text = secretbox::seal(b"hello, everyone!", &nonce, &key);
+    let decrypted = secretbox::open(&cipher_text, &nonce, &key).unwrap();
+    println!("{}", String::from_utf8(decrypted).unwrap());
+}
+
+#[cfg(test)]
+mod tests {
+    use pass_db;
+    use pass_db::PasswordDb;
+
+    #[test]
+    fn read_scenario() {
+        // decrypt the db file
+        const path: &str = "pwdb.sqlite.crypt";
+        const decrypt_key: &str = "test123";
+        const decrypted_path: &str = "pwdb.sqlite";
+        // crypto::decrypt_file(path, decrypt_key, decrypted_path);
+
+        // query the decrypted db for the desired site
+        const site: &str = "friendster.com";
+        let password_db = pass_db::open_db(decrypted_path);
+        let user_credentials = password_db.find_for_site(site);
+    }
+
+    #[test]
+    fn write_scenario() {
+        // decrypt the db file
+        const path: &str = "pwdb.sqlite.crypt";
+        const decrypt_key: &str = "test123";
+        const decrypted_path: &str = "pwdb.sqlite";
+        // crypto::decrypt_file(path, decrypt_key, decrypted_path);
+
+        // query the decrypted db for the desired site
+        const site: &str = "friendster.com";
+        let mut password_db = pass_db::open_db(decrypted_path);
+        let result = password_db.insert_for_site(site, "username", "pw");
+        result.expect("insert failed");
+    }
+}
---
+++ src/pass_db.rs
@@ -0,0 +1,63 @@
+fn testing() -> Result<(), Box<std::error::Error>> {
+    let conn = rusqlite::Connection::open("pwdb.sqlite").expect("failed to open connection");
+
+    conn.execute("DROP TABLE IF EXISTS credentials", rusqlite::NO_PARAMS)
+        .expect("failed to drop table");
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS credentials (
+        id          ROWID INTEGER PRIMARY KEY,
+        site        TEXT NOT NULL,
+        username    TEXT NOT NULL,
+        password    TEXT NOT NULL
+    )",
+        rusqlite::NO_PARAMS,
+    ).expect("failed to create table.");
+
+    conn.execute(
+        "INSERT INTO credentials (site, username, password) VALUES ('facebook', 'andysalerno', 'password123')",
+        rusqlite::NO_PARAMS,
+    ).expect("insert failed");
+
+    let my_row: String = conn
+        .query_row("SELECT * FROM credentials", rusqlite::NO_PARAMS, |row| {
+            row.get(1)
+        }).expect("failed to run query");
+
+    println!("{}", my_row);
+
+    Ok(())
+}
+
+pub trait PasswordDb {
+    fn find_for_site(&self, site: &str) -> String;
+    fn insert_for_site(&mut self, site: &str, username: &str, password: &str) -> Result<(), ()>;
+}
+
+pub struct SqlitePasswordDb;
+
+impl PasswordDb for SqlitePasswordDb {
+    fn find_for_site(&self, site: &str) -> String {
+        return "my_pwd_123".to_owned();
+    }
+
+    fn insert_for_site(&mut self, site: &str, username: &str, password: &str) -> Result<(), ()> {
+        Ok(())
+    }
+}
+
+pub struct TestingPasswordDb;
+
+impl PasswordDb for TestingPasswordDb {
+    fn find_for_site(&self, site: &str) -> String {
+        return "my_pwd_123".to_owned();
+    }
+
+    fn insert_for_site(&mut self, site: &str, username: &str, password: &str) -> Result<(), ()> {
+        Ok(())
+    }
+}
+
+pub fn open_db(path: &str) -> impl PasswordDb {
+    TestingPasswordDb {}
+}
